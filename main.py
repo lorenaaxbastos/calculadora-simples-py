5f99e880e7da5170c3e93fe0816f871066ab859e
Lorena Bastos <lorenaax.bastos@hotmail.com>
2024-02-15 18:27:45 -0300

    feat operacoes de soma, multiplicacao e divisao

---
+++ main.py
@@ -0,0 +1,20 @@
+class Calculadora:
+
+    def __init__(self, n1, n2):
+        self._n1 = n1
+        self._n2 = n2
+
+    @property
+    def soma(self):
+        return self._n1 + self._n2
+
+    @property
+    def multiplicacao(self):
+        return self._n1 * self._n2
+
+    @property
+    def divisao(self):
+        try:
+            return self._n1 / self._n2
+        except ZeroDivisionError:
+            return "Não é possível dividir por zero."
